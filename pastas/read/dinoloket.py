3c9a72af8fa77b3515f7dcfe560302c4dd391d39
Steijn van <steit1@bw.local>
2020-06-30 10:50:17 +0200

    DinoPeilschaal for reading river gauge levels

--- pastas/read/dinoloket.py
+++ pastas/read/dinoloket.py
@@ -5,14 +5,18 @@ data from dinoloket.nl.
 TODO: Get rid of filternummer en opmerking in self.series
 
 """
+from itertools import takewhile
+from io import StringIO
+import csv
+import os
 
 import numpy as np
-from pandas import Series, read_csv
+import pandas as pd
 
 from ..timeseries import TimeSeries
 
 
-def read_dino(fname, variable='Stand_cm_tov_NAP', factor=0.01):
+def read_dino(fname, variable='stand', factor=0.01, settings='oseries', epsg=28992):
     """This method can be used to import files from Dinoloket that contain
      groundwater level measurements (https://www.dinoloket.nl/)
 
@@ -31,187 +35,232 @@ def read_dino(fname, variable='Stand_cm_tov_NAP', factor=0.01):
     """
 
     # Read the file
-    dino = DinoGrondwaterstand(fname)
-    ts = []
-
-    if variable not in dino.data.keys():
-        raise (
-            ValueError("variable %s is not in this dataset. Please use one of "
-                       "the following keys: %s" % (
-                           variable, dino.data.keys())))
-    series = dino.data[variable] * factor  # To make it meters)
-    if len(dino.meta) > 0:
-        metadata = dino.meta[-1]
+    is_river_gauge = os.path.basename(fname).lower().startswith('p')
+    if is_river_gauge:
+        dino = DinoPeilschaal.from_file(fname)
     else:
-        metadata = None
-
-    metadata['x'] = dino.x
-    metadata['y'] = dino.y
-    metadata['z'] = np.mean((dino.bovenkant_filter, dino.onderkant_filter))
-    metadata['projection'] = 'epsg:28992'
-
-    ts.append(TimeSeries(series,
-                         name=dino.locatie + '_' + str(dino.filternummer),
-                         metadata=metadata, settings='oseries'))
-    if len(ts) == 1:
-        ts = ts[0]
+        dino = DinoGrondwaterstand.from_file(fname)
+      
+    # create timeseries object
+    ts = TimeSeries(dino.get_ts_series(variable, factor),
+        name=dino.get_ts_name(), 
+        metadata=dino.get_ts_meta(factor, epsg).to_dict(),
+        settings=settings,
+        )    
     return ts
+        
 
+class DinoDataset:
+    # class attributes to be filled in subclass
+    meta_index_col = []
+    series_index_col = []
+    group_meta = []
+    group_series = []
+    meta_cols = {}
+    meta_level_cols = []
+    series_cols = {}
+    series_level_cols = []
 
-class DinoGrondwaterstand:
-    def __init__(self, fname):
-        with open(fname, 'r') as f:
-            # lees de header
-            line = f.readline()
-            header = dict()
-            while line not in ['\n', '', '\r\n']:
-                propval = line.split(',')
-                prop = propval[0]
-                prop = prop.replace(':', '')
-                prop = prop.strip()
-                val = propval[1]
-                if propval[2] != '':
-                    val = val + ' ' + propval[2].replace(':', '') + ' ' + \
-                          propval[3]
-                header[prop] = val
-                line = f.readline()
+    def __init__(self, header, meta, series):
+        self.header = header
+        self.meta = meta
+        self.series = series
 
-            # lees gat
-            while (line == '\n') or (line == '\r\n'):
-                line = f.readline()
+    @staticmethod
+    def skip_blanks(line, reader):
+        while (line is None) or (len(line) == 0):
+            try:
+                line = next(reader)
+            except StopIteration:
+                return
+        return line
+    
+    @staticmethod
+    def read_header(line, reader, header):
+        while (len(line) > 0) and not line[0].startswith('Locatie'):
+            key = line[0].rstrip(':').strip()
+            values = line[1:]
+            header[key] = values
+            line = next(reader)
+        return line
 
-            # lees referentieniveaus
-            ref = dict()
-            while line not in ['\n', '', '\r\n']:
-                propval = line.split(',')
-                prop = propval[0]
-                prop = prop.replace(':', '')
-                prop = prop.strip()
-                if len(propval) > 1:
-                    val = propval[1]
-                    ref[prop] = val
-                line = f.readline()
+    @staticmethod
+    def try_get(series, variable):
+        try:
+            return (series
+                .loc[:, variable]
+                )
+        except KeyError:
+            raise ValueError(
+                "variable {var:} is not in this dataset. Please use one of "
+                "the following keys: {keys:}".format(
+                var=variable,
+                keys=series.columns.tolist(),
+                ))
 
-            # lees gat
-            while (line == '\n') or (line == '\r\n'):
-                line = f.readline()
+    @classmethod
+    def read_meta(cls, meta_header, reader, delimiter):
+        meta_header = [c for c in meta_header if c]
+        not_empty = lambda row: any(bool(r) for r in row)
+        meta_rows = takewhile(not_empty, reader)
+        meta_f = StringIO(
+            '\n'.join(delimiter.join(r) for r in meta_rows)
+            )
+        return pd.read_csv(meta_f,
+            delimiter=delimiter,
+            index_col=cls.meta_index_col,
+            header=None,
+            names=meta_header,
+            parse_dates=True,
+            dayfirst=True,
+            usecols=meta_header,
+            )
 
-            # lees meta-informatie
-            metaList = list()
-            line = line.strip()
-            properties = line.split(',')
-            line = f.readline()
-            while line not in ['\n', '', '\r\n']:
-                meta = dict()
-                line = line.strip()
-                values = line.split(',')
-                for i, value in enumerate(values):
-                    meta[properties[i]] = value
-                metaList.append(meta)
-                line = f.readline()
+    @classmethod
+    def read_series(cls, series_header, f, delimiter):
+        series_header = [c for c in series_header if c]
+        series = pd.read_csv(f,
+            delimiter=delimiter,
+            index_col=cls.series_index_col,
+            header=None,
+            names=series_header,
+            parse_dates=True,
+            dayfirst=True,
+            usecols=series_header,
+            )
+        return series
 
-            # lees gat
-            while (line == '\n') or (line == '\r\n'):
-                line = f.readline()
+    @classmethod
+    def from_file(cls, fname, delimiter=','):
+        with open(fname) as f:
+            reader = csv.reader(f, delimiter=delimiter)
+            line = next(reader)
 
-            line = line.strip()
-            titel = line.split(',')
-            while '' in titel:
-                titel.remove('')
+            # read header
+            header = {}
+            line = cls.read_header(line, reader, header)
 
-            # lees reeksen
-            if line != '':
-                # Validate if titles are valid names
-                validator = np.lib._iotools.NameValidator()
-                titel = validator(titel)
-                dtype = [np.float64] * (len(titel))
-                dtype[0] = "S11"
-                dtype[1] = np.int
-                dtype[2] = "S10"
-                dtype[titel.index('Bijzonderheid')] = object
-                dtype[titel.index('Opmerking')] = object
-                dtype = list(zip(titel, dtype))
+            # skip blanks
+            line = cls.skip_blanks(line, reader)
 
-                usecols = range(0, len(titel))
-                # # usecols.remove(2)
-                measurements = read_csv(f, header=None, names=titel,
-                                        parse_dates=['Peildatum'],
-                                        index_col='Peildatum',
-                                        dayfirst=True,
-                                        usecols=usecols)
-                ts = measurements['Stand_cm_tov_NAP']
-                #
-                # measurements = np.genfromtxt(f, delimiter=',',
-                #                              dtype=None,
-                #                              usecols=range(0, len(titel)),
-                #                              names=titel,
-                #                              missing_values=[''])
-                # values = measurements['Stand_cm_tov_NAP'] / float(100)
-                # values[values == -0.01] = np.NAN
+            # read header (abbrevations)           
+            line = cls.read_header(line, reader, header)
 
-                # measurements = np.genfromtxt(fname, delimiter=',', dtype=dtype,
-                #                              names=titel, usecols=usecols)
-                # values = measurements['Stand_cm_tov_NAP'] / 100
-
-                # %% zet de ingelezen data om in een reeks
-                # if measurements['Stand_cm_tov_NAP'].dtype == np.dtype('bool'):
-                #     # wanneer bleek dat het allemaal lege waarden waren
-                #     if values.size == 1:
-                #         values = np.NAN
-                #     else:
-                #         values[:] = np.NAN
-
-                # if measurements['Peildatum'].size == 1:
-                #     datum = pd.to_datetime(
-                #         measurements['Peildatum'].item(), dayfirst=True)
-                #     ts = pd.Series([values], [datum])
-                # else:
-                #     datum = pd.to_datetime(measurements['Peildatum'])
-                #     ts = pd.Series(values, datum)
+            # skip blanks
+            meta_header = cls.skip_blanks(line, reader)       
+                
+            # read metadata
+            if meta_header is not None:
+                meta = cls.read_meta(meta_header, reader, delimiter)
             else:
-                measurements = None
-                ts = Series()
+                meta = None   
 
-            # %% kies welke invoer opgeslagen wordt
-            self.meta = metaList
-            if self.meta:
-                self.locatie = self.meta[-1]['Locatie']
-                self.filternummer = int(float(self.meta[-1]['Filternummer']))
-                self.x = float(self.meta[-1]['X-coordinaat'])
-                self.y = float(self.meta[-1]['Y-coordinaat'])
-                meetpunt = self.meta[-1]['Meetpunt (cm t.o.v. NAP)']
-                if meetpunt == '':
-                    self.meetpunt = np.nan
-                else:
-                    self.meetpunt = float(meetpunt) / 100
-                maaiveld = self.meta[-1]['Maaiveld (cm t.o.v. NAP)']
-                if maaiveld == '':
-                    self.maaiveld = np.nan
-                else:
-                    self.maaiveld = float(maaiveld) / 100
-                bovenkant_filter = self.meta[-1][
-                    'Bovenkant filter (cm t.o.v. NAP)']
-                if bovenkant_filter == '':
-                    self.bovenkant_filter = np.nan
-                else:
-                    self.bovenkant_filter = float(bovenkant_filter) / 100
-                self.onderkant_filter = self.meta[-1][
-                    'Onderkant filter (cm t.o.v. NAP)']
-                if self.onderkant_filter == '':
-                    self.onderkant_filter = np.nan
-                else:
-                    self.onderkant_filter = float(self.onderkant_filter) / 100
+            # skip blanks
+            try:
+                series_header = cls.skip_blanks(None, reader)
+            except StopIteration:
+                series_header = None
+
+            # read series
+            if series_header is not None:
+                series = cls.read_series(series_header, f, delimiter)
             else:
-                # de metadata is leeg
-                self.locatie = ''
-                self.filternummer = np.nan
-                self.x = np.nan
-                self.y = np.nan
-                self.meetpunt = np.nan
-                self.maaiveld = np.nan
-                self.bovenkant_filter = np.nan
-                self.onderkant_filter = np.nan
-            self.data = measurements
-            self.stand = ts
+                series = None
+            
+            return cls(header, meta, series)
+                    
+    def get_ts_meta(self, factor, epsg=None):
+        if self.meta is None:
+            return {k: np.nan for k in self.meta_cols}
+        meta = (self.meta
+            .groupby(level=self.group_meta)
+            .last()
+            .rename(columns={v: k for k, v in self.meta_cols.items()})
+            .loc[:, [k for k in self.meta_cols]]            
+            .iloc[0, :] 
+            )
+        meta.loc[self.meta_level_cols] *= factor
 
-        f.close()
+        if epsg is not None:
+            meta.loc['projection'] = 'epsg:{:d}'.format(epsg)
+
+        return meta
+
+    def get_ts_series(self, variable, factor):
+        if self.series is None:
+            return pd.Series()
+        series = (self.series
+            .groupby(level=self.group_series)
+            .first()
+            .rename(columns={v: k for k, v in self.series_cols.items()})
+            )
+        series = self.try_get(series, variable)
+        series *= factor
+
+        return series
+
+
+class DinoGrondwaterstand(DinoDataset):
+    meta_index_col = [0, 1]
+    series_index_col = [0, 1, 2]
+    group_meta = [0, 1]
+    group_series = [2,]
+
+    meta_cols = {
+        'x': 'X-coordinaat',
+        'y': 'Y-coordinaat',
+        'meetpunt': 'Meetpunt (cm t.o.v. NAP)',
+        'maaiveld': 'Maaiveld (cm t.o.v. NAP)',
+        'bovenkant_filter': 'Bovenkant filter (cm t.o.v. NAP)',
+        'onderkant_filter': 'Onderkant filter (cm t.o.v. NAP)',
+        }
+    meta_level_cols = [
+        'meetpunt', 'maaiveld', 'bovenkant_filter', 'onderkant_filter'
+        ]
+
+    series_cols = {
+        'stand': 'Stand (cm t.o.v. NAP)',
+        }
+    series_level_cols = [
+        'stand',
+        ]
+
+    def get_ts_name(self):
+        loc, filt = self.meta.index[0]
+        return '{loc:}_{filt:}'.format(
+            loc=loc,
+            filt=filt,
+            )
+            
+    def get_ts_meta(self, factor, epsg=None):
+        meta = super().get_ts_meta(factor, epsg)
+        meta.loc['z'] = (meta
+            .loc[['bovenkant_filter', 'onderkant_filter']]
+            .mean()
+            )
+        return meta
+
+
+class DinoPeilschaal(DinoDataset):
+    meta_index_col = [0,]
+    series_index_col = [0, 1]
+    group_meta = [0,]
+    group_series = [1,]
+
+    meta_cols = {
+        'x': 'X-coordinaat',
+        'y': 'Y-coordinaat',
+        }
+
+    series_cols = {
+        'stand': 'Stand (cm t.o.v. NAP)',
+        }
+    series_level_cols = [
+        'stand',
+        ]
+
+    def get_ts_name(self):
+        loc = self.meta.index[0]
+        return '{loc:}'.format(
+            loc=loc,
+            )
